fe64198e1c23980dd4d33c596c00a20ca670e4e2
tejasmishra <tej.mishra@gmail.com>
2015-07-16 16:51:16 +0530

    ko binding

---
+++ js/app.js
@@ -0,0 +1,12 @@
+var ViewModal = function() {
+	this.clickCount = ko.observable(0);
+	this.name = ko.observable("Tabby");
+	this.imgSrc = ko.observable("img/434164568_fea0ad4013_z.jpg");
+	this.imgAttribution = ko.observable("http://www.google.com");
+
+	this.incrementCounter = function() {
+		this.clickCount(this.clickCount() + 1);
+	};
+}
+
+ko.applyBindings(new ViewModal());
